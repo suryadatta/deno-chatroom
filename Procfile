557e32a0627d052259ceb6f29ce2456dfdb87677
suryadatta <suryadatta1@gmail.com>
2020-07-01 03:44:41 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: deno run --allow-read --allow-net=:${PORT} app.ts --port=${PORT}
---
+++ app.ts
@@ -0,0 +1,33 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+import { acceptWebSocket, acceptable } from "https://deno.land/std/ws/mod.ts";
+import { chatConnection } from "./ws/chatroom.ts";
+import { parse } from "https://deno.land/std/flags/mod.ts";
+
+const { args } = Deno;
+const DEFAULT_PORT = 3000;
+const argPort = parse(args).port;
+// server setup
+const server = serve({ port: argPort ? Number(argPort) : DEFAULT_PORT });
+console.log("http://localhost:3000/");
+
+for await (const req of server) {
+  // serve index page
+  if (req.url === "/") {
+    req.respond({
+      status: 200,
+      body: await Deno.open("./public/index.html"),
+    });
+  }
+
+  // accept the websocket connection
+  if (req.url === "/ws") {
+    if (acceptable(req)) {
+      acceptWebSocket({
+        conn: req.conn,
+        bufReader: req.r,
+        bufWriter: req.w,
+        headers: req.headers,
+      }).then(chatConnection);
+    }
+  }
+}
